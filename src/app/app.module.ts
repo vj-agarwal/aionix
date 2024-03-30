52240a8c8b1e87457cb9390931149bcc74fac90d
Vijay Agarwal <ervjagarwal@gmail.com>
2024-03-30 22:42:01 +0530

    Initial Commit for Aionix Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextBoxComponent } from './material_attributes/text-box/text-box.component';
+import { TextAreaComponent } from './material_attributes/text-area/text-area.component';
+import { ToggleComponent } from './material_attributes/toggle/toggle.component';
+import { ButtonComponent } from './material_attributes/button/button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextBoxComponent,
+    TextAreaComponent,
+    ToggleComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material_attributes/text-box/text-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-text-box',
+  templateUrl: './text-box.component.html',
+  styleUrls: ['./text-box.component.css']
+})
+export class TextBoxComponent {
+  value = 'Clear me';
+}
